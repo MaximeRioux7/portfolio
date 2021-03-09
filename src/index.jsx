2b0c1425eabd401b791e3072ee2b0f51cddc7d6b
QcDiablo <74427389+QcDiablo@users.noreply.github.com>
2021-03-08 22:51:13 -0500

    Github Pages

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.scss';
 import App from './components/App';
-import {Router} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import History from './components/History';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Router history={History} basename="/portfolio">
+		<BrowserRouter basename="/portfolio">
 			<ScrollToTop />
 			<App />
-		</Router>
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
